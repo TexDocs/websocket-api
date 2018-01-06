09cdc504604a1a7300d53d7e87f87f30bb316035
Til Blechschmidt <til@blechschmidt.de>
2018-01-06 13:23:19 +0100

    Reworked identifiers with binary structure

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,9 +1,17 @@
 pub const PROTOCOL_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-pub const HANDSHAKE: u8             = 0;
-pub const HANDSHAKE_ACK: u8         = 1;
-pub const HANDSHAKE_ERR: u8         = 2;
-pub const PROJECT_REQUEST: u8       = 3;
-pub const PROJECT_REQUEST_ERR: u8   = 4;
-pub const PROJECT: u8               = 5;
-pub const FILE_TREE: u8             = 6;
+// Handshake
+pub const HANDSHAKE: u8             = 0b00000_111;
+pub const HANDSHAKE_ACK: u8         = 0b00000_000;
+pub const HANDSHAKE_ERR: u8         = 0b00000_100;
+
+// Project
+pub const PROJECT_REQUEST: u8       = 0b00001_111;
+pub const PROJECT: u8               = 0b00001_000;
+pub const FILE_TREE: u8             = 0b00001_001;
+pub const PROJECT_REQUEST_ERR: u8   = 0b00001_100;
+
+// File
+pub const FILE_REQUEST: u8          = 0b00010_111;
+pub const FILE: u8                  = 0b00010_000;
+pub const FILE_REQUEST_ERR: u8      = 0b00010_100;
